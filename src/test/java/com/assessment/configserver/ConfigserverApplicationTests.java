8bc30337e916574d462e2f380c2c9156c5c4d2ef
Rahul Karodia <rahulkarodia28@gmail.com>
2020-11-06 08:35:35 -0500

    Config Server First try

---
+++ src/test/java/com/assessment/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
